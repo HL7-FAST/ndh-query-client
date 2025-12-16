21fb6e6fb901893611d57f7aaac60dc9ad5f1136
bstewartlg <114590619+bstewartlg@users.noreply.github.com>
2025-12-16 12:14:40 -0500

    Correct count for networks

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,64 +11,63 @@
 require 'json'
 
 class WelcomeController < ApplicationController
-
   # GET /
 
   def index
-    connect_to_server 
+    connect_to_server
     get_resource_counts
   end
 
   def get_resource_counts
-    begin
-      @endpoints = 0
-      @healthCareServices = 0
-      @insurancePlans = 0
-      @locations = 0
-      @networks = 0
-      @organizations = 0
-      @organizationAffiliations = 0
-      @practitioners = 0
-      @practitionerRoles = 0
+    @endpoints = 0
+    @healthCareServices = 0
+    @insurancePlans = 0
+    @locations = 0
+    @networks = 0
+    @organizations = 0
+    @organizationAffiliations = 0
+    @practitioners = 0
+    @practitionerRoles = 0
 
-		 	response = RestClient::Request.new( :method => :get, :url => server_url + "/$get-resource-counts").execute
-      results = JSON.parse(response.to_str)
-      results["parameter"].each do |param|
-        case param["name"]
-        when "Endpoint"
-          @endpoints = param["valueInteger"]
-        when "HealthcareService"
-          @healthCareServices = param["valueInteger"]
-        when "InsurancePlan"
-          @insurancePlans = param["valueInteger"]
-        when "Location"
-          @locations = param["valueInteger"]
-        when "Network"
-          @networks = param["valueInteger"]
-        when "Organization"
-          @organizations = param["valueInteger"]
-        when "OrganizationAffiliation"
-          @organizationAffiliations = param["valueInteger"]
-        when "Practitioner"
-          @practitioners = param["valueInteger"]
-        when "PractitionerRole"
-          @practitionerRoles = param["valueInteger"]
-        end
+    response = RestClient::Request.new(method: :get, url: server_url + '/$get-resource-counts').execute
+    results = JSON.parse(response.to_str)
+    results['parameter'].each do |param|
+      case param['name']
+      when 'Endpoint'
+        @endpoints = param['valueInteger']
+      when 'HealthcareService'
+        @healthCareServices = param['valueInteger']
+      when 'InsurancePlan'
+        @insurancePlans = param['valueInteger']
+      when 'Location'
+        @locations = param['valueInteger']
+      # when "Network"
+      #   @networks = param["valueInteger"]
+      when 'Organization'
+        @organizations = param['valueInteger']
+      when 'OrganizationAffiliation'
+        @organizationAffiliations = param['valueInteger']
+      when 'Practitioner'
+        @practitioners = param['valueInteger']
+      when 'PractitionerRole'
+        @practitionerRoles = param['valueInteger']
       end
+    end
 
-  
-    rescue => exception
-      @endpoints = 0
-      @healthCareServices = 0
-      @insurancePlans = 0
-      @locations = 0
-      @networks = 0
-      @organizations = 0
-      @organizationAffiliations = 0
-      @practitioners = 0
-      @practitionerRoles = 0
-		end
-
+    # Network isn't a resource so we will query for its count separately
+    response = RestClient::Request.new(method: :get,
+                                       url: "#{server_url}/Organization?type=ntwk&_total=accurate").execute
+    results = JSON.parse(response.to_str)
+    @networks = results['total']
+  rescue StandardError => e
+    @endpoints = 0
+    @healthCareServices = 0
+    @insurancePlans = 0
+    @locations = 0
+    @networks = 0
+    @organizations = 0
+    @organizationAffiliations = 0
+    @practitioners = 0
+    @practitionerRoles = 0
   end
-
 end
